ad5ad88ff8aa5ebe51a0a6fdab0c4fe2fcdaabbb
NikDimer <60222621+NikDimer@users.noreply.github.com>
2024-11-28 14:08:52 +0300

    BigUpdate#1
    Оптимизировал анимацию облаков в Hero(из-за них всё лагало), обновил информацию в MyPath и некоторые медиа-запросы подправил

---
+++ src/components/Hero/CloudsAnimation/CloudsAnimation.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import cloud1 from "../../../assets/images/cloud1.png";
+import cloud2 from "../../../assets/images/cloud2.png";
+import cloud3 from "../../../assets/images/cloud3.png";
+import cloud4 from "../../../assets/images/cloud4.png";
+import cloud5 from "../../../assets/images/cloud5.png";
+import cloud6 from "../../../assets/images/cloud6.png";
+import cloud7 from "../../../assets/images/cloud7.png";
+import cloud8 from "../../../assets/images/cloud8.png";
+import cloud9 from "../../../assets/images/cloud9.png";
+
+const cloudsData = [
+    { src: cloud1, left: 0, top: "36%", speed: 20, delay: 0 },
+    { src: cloud2, left: 0, top: "11%", speed: 20, delay: 3 },
+    { src: cloud3, left: 0, top: "21%", speed: 20, delay: 7 },
+    { src: cloud4, left: 0, top: "20%", speed: 20, delay: 11 },
+    { src: cloud5, left: 0, top: "40%", speed: 20, delay: 14 },
+    { src: cloud6, left: 0, top: "25%", speed: 20, delay: 19 },
+    { src: cloud7, left: 0, top: "45%", speed: 20, delay: 23 },
+    { src: cloud8, left: 0, top: "26%", speed: 20, delay: 25 },
+    { src: cloud9, left: 0, top: "50%", speed: 20, delay: 28 },
+];
+
+const Cloud = ({ src, speed, left, top, sizeMultiplier, delay }) => {
+    const [windowWidth, setWindowWidth] = useState(1400);
+
+    useEffect(() => {
+        setWindowWidth(window.innerWidth);
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <motion.img
+            src={src}
+            alt="cloud"
+            initial={{ x: windowWidth + 300 + left }}
+            animate={{
+                x: -600 + left,
+                transition: {
+                    duration: speed,
+                    delay: delay,
+                    repeat: Infinity,
+                    ease: "linear",
+                },
+            }}
+            style={{
+                position: "absolute",
+                top: top,
+                width: `${292 * sizeMultiplier}px`,
+                height: "auto",
+                zIndex: -1,
+                opacity: 0.8,
+            }}
+        />
+    );
+};
+
+const CloudAnimation = () => {
+    const [sizeMultiplier, setSizeMultiplier] = useState(1);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSizeMultiplier(
+                window.innerWidth <= 980
+                    ? 0.5
+                    : window.innerWidth <= 1600
+                    ? 0.75
+                    : 1
+            );
+        };
+        handleResize(); // Установить начальное значение
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <div
+            style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                overflow: "hidden",
+            }}
+        >
+            {cloudsData.map((cloud, index) => (
+                <Cloud
+                    key={index}
+                    src={cloud.src.src}
+                    left={cloud.left}
+                    top={cloud.top}
+                    speed={cloud.speed}
+                    sizeMultiplier={sizeMultiplier}
+                    delay={cloud.delay}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default CloudAnimation;
--- src/components/MyPath/MyPath.jsx
+++ src/components/MyPath/MyPath.jsx
@@ -1,13 +1,13 @@
-import styles from './styles.module.css'
-import Image from 'next/image'
-import classnames from 'classnames'
+import styles from "./styles.module.css";
+import Image from "next/image";
+import classnames from "classnames";
 
-import path1 from '../../../src/assets/images/path1.jpg'
-import path2 from '../../../src/assets/images/path2.jpeg'
-import path3 from '../../../src/assets/images/path3.jpg'
-import path4 from '../../../src/assets/images/path4.jpg'
+import path1 from "../../../src/assets/images/path1.jpg";
+import path2 from "../../../src/assets/images/path2.jpg";
+import path3 from "../../../src/assets/images/path3.png";
+import path4 from "../../../src/assets/images/path4.jpg";
 
-import train from '../../../src/assets/images/train.png'
+import train from "../../../src/assets/images/train.png";
 
 //import TrainWay from '@/src/assets/images/TrainWay.png'
 
@@ -17,47 +17,142 @@ export default function MyPath() {
             <h1 className={styles.MyPathTitle}>Мой путь становления</h1>
             <div className={styles.MyPathTrainWayContainer}>
                 <div className={styles.MyPathTrainWay}>
-                    <div className={classnames(styles.MyPathTrainWayCircle1, styles.MyPathTrainWayCircle)}>
-                        <Image src={path1} width={200} height={200} className={styles.MyPathTrainWayImage} />
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayCircle1,
+                            styles.MyPathTrainWayCircle
+                        )}
+                    >
+                        <Image
+                            src={path1}
+                            width={200}
+                            height={200}
+                            className={styles.MyPathTrainWayImage}
+                        />
                     </div>
-                    <div className={classnames(styles.MyPathTrainWayTextContainer1, styles.MyPathTrainWayTextContainer)}>
-                        <p className={styles.MyPathTrainWayText} >1.Я начал свой путь в сайтостроении при помощи HtmlAcademy, где изучил HTML, CSS, JS, немного PHP.</p>
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayTextContainer1,
+                            styles.MyPathTrainWayTextContainer
+                        )}
+                    >
+                        <p className={styles.MyPathTrainWayText}>
+                            1. Я начал свой путь в сайтостроении при помощи
+                            HtmlAcademy, где изучил HTML, CSS, JS, немного PHP.
+                        </p>
                     </div>
-                    <div className={classnames(styles.MyPathTrainWayLine1, styles.MyPathTrainWayLine)}>
-                        <Image src={train} className={styles.MyPathTrainImage1} />
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayLine1,
+                            styles.MyPathTrainWayLine
+                        )}
+                    >
+                        <Image
+                            src={train}
+                            className={styles.MyPathTrainImage1}
+                        />
                     </div>
 
-                    <div className={classnames(styles.MyPathTrainWayCircle2, styles.MyPathTrainWayCircle)}>
-                        <Image src={path2} width={200} height={200} className={styles.MyPathTrainWayImage} />
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayCircle2,
+                            styles.MyPathTrainWayCircle
+                        )}
+                    >
+                        <Image
+                            src={path2}
+                            width={200}
+                            height={200}
+                            className={styles.MyPathTrainWayImage}
+                        />
                     </div>
-                    <div className={classnames(styles.MyPathTrainWayTextContainer2, styles.MyPathTrainWayTextContainer)}><p className={styles.MyPathTrainWayText} >
-                        2.В дальнейшем я улучшал свои навыки вёрстки и начал делать первые шаги в изучении фреймворков.</p>
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayTextContainer2,
+                            styles.MyPathTrainWayTextContainer
+                        )}
+                    >
+                        <p className={styles.MyPathTrainWayText}>
+                            2. Прошёл на Интенсив Яндекс Академии, где получил
+                            начальное понимание React и вектор для дальнейшего
+                            развития.
+                        </p>
                     </div>
-                    <div className={classnames(styles.MyPathTrainWayLine2, styles.MyPathTrainWayLine)}>
-                        <Image src={train} className={styles.MyPathTrainImage2} />
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayLine2,
+                            styles.MyPathTrainWayLine
+                        )}
+                    >
+                        <Image
+                            src={train}
+                            className={styles.MyPathTrainImage2}
+                        />
                     </div>
 
-                    <div className={classnames(styles.MyPathTrainWayCircle3, styles.MyPathTrainWayCircle)}>
-                        <Image src={path3} width={200} height={200} className={styles.MyPathTrainWayImage} />
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayCircle3,
+                            styles.MyPathTrainWayCircle
+                        )}
+                    >
+                        <Image
+                            src={path3}
+                            width={200}
+                            height={200}
+                            className={styles.MyPathTrainWayImage}
+                        />
                     </div>
-                    <div className={classnames(styles.MyPathTrainWayTextContainer3, styles.MyPathTrainWayTextContainer)}>
-                        <p className={styles.MyPathTrainWayText} >3.Прошёл на Интенсив Яндекс Академии, где получил начальное понимание React и вектор для дальнейшего развития.</p>
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayTextContainer3,
+                            styles.MyPathTrainWayTextContainer
+                        )}
+                    >
+                        <p className={styles.MyPathTrainWayText}>
+                            3. В дальнейшем постоянно изучал материалы по React,
+                            JavaScript и Next.js, подкрепляя практикой
+                        </p>
                     </div>
-                    <div className={classnames(styles.MyPathTrainWayLine3, styles.MyPathTrainWayLine)}>
-                        <Image src={train} className={styles.MyPathTrainImage3} />
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayLine3,
+                            styles.MyPathTrainWayLine
+                        )}
+                    >
+                        <Image
+                            src={train}
+                            className={styles.MyPathTrainImage3}
+                        />
                     </div>
 
-                    <div className={classnames(styles.MyPathTrainWayCircle4, styles.MyPathTrainWayCircle)}>
-                        <Image src={path4} width={200} height={200} className={styles.MyPathTrainWayImage} />
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayCircle4,
+                            styles.MyPathTrainWayCircle
+                        )}
+                    >
+                        <Image
+                            src={path4}
+                            width={200}
+                            height={200}
+                            className={styles.MyPathTrainWayImage}
+                        />
                     </div>
-                    <div className={classnames(styles.MyPathTrainWayTextContainer4, styles.MyPathTrainWayTextContainer)}><p className={styles.MyPathTrainWayText} >
-                        4.С тех пор я активно занимался самообучением с помощью лекций и статей в интернете, ориентируясь по советам других опытных разработчиков.</p>
+                    <div
+                        className={classnames(
+                            styles.MyPathTrainWayTextContainer4,
+                            styles.MyPathTrainWayTextContainer
+                        )}
+                    >
+                        <p className={styles.MyPathTrainWayText}>
+                            4. На данный момент достиг Junior-уровня, нахожусь в
+                            активном поиске работы, стажировок и других
+                            возможностей набраться опыта
+                        </p>
                     </div>
                 </div>
             </div>
-            <div className={styles.MyPathEndTextContainer}>
-                <p className={styles.MyPathEndText}>Сейчас я стараюсь укрепить теоретические основы и ищу как можно больше возможности для реальной практики, чтобы собрать себе полноценное портфолио.</p>
-            </div>
         </div>
-    )
+    );
 }
